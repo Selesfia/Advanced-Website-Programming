4b746587b386cbd5728db9aa90695475eeea7f80
Selesfia <jesslinehu007@gmail.com>
2024-01-12 19:42:17 +0800

    commit

--- HomeWork/HW4/people.js
+++ HomeWork/HW4/people.js
@@ -1,85 +1,95 @@
-import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
- 
-const peoples = new Map();
-peoples.set("Jessline", {
-  account: "Jessline",
-  password: "011002",
-});
-peoples.set("Celine", {
-  account: "Celine",
-  password: "041100",
-});
+import{Application, Router, send} from "https://deno.land/x/oak/mod.ts";
+
+const peoples = new Map()
+    peoples.set('Jessline',{
+        name: "Jessline",
+        tel: "0937711339",
+        account: "Jessline@gmail.com",
+        password: "011002",
+        gender: "female",
+    });
+    peoples.set('Joshua',{
+        name: "Joshua",
+        tel: "0971660435",
+        account: "Joshua@gmail.com",
+        password: "301295",
+        gender: "male",
+    });
 
 const router = new Router();
 router
-  .get("/", (ctx) => {
-    ctx.response.body = "Welcome!";
-  })
-  .get("/public/(.*)", async (ctx) => {
-    let wpath = ctx.params[0]
-    //console.log('wpath=', wpath)
-    await send(ctx, wpath, {
-      root: Deno.cwd()+"/public/",
-      index: "index.html",
+    .get('/',(ctx) =>{
+        ctx.response.redirect("http://127.0.0.1:8000/public/index.html")
     })
-  })
-  .post("/people/login", async (ctx) => {
-    const body = ctx.request.body()
-    if (body.type === "form") {
-      const pairs = await body.value
-      //console.log('pairs=', pairs)
-      const params = {}
-      for (const [key, value] of pairs) {
-        params[key] = value
-      }
-      console.log('params=', params)
-      let account = params['account']
-      let password = params['password']
-      //console.log(`account=${account} password=${password}`)
-      ctx.response.type = 'text/html'
-      if (peoples.has(account) && peoples.get(account).password == password) {
-        ctx.response.body = `
-          <h1>Login Success</h1>
-          <h2><a href="/public/index.html">Enter the system</a></h2>`
-      } else {
-        ctx.response.body = `
-          <h1>Login Failed. Please check whether the information you input are correct.</h1>`
-      }
-    }
-  })
-  .post("/people/signup", async (ctx) => {
-    const body = ctx.request.body()
-    if (body.type === "form") {
-      const pairs = await body.value
-      console.log('pairs=', pairs)
-      const params = {}
-      for (const [key, value] of pairs) {
-        params[key] = value
-      }
-      console.log('params=', params)
-      let account = params['account']
-      let password = params['password']
-      console.log(`account=${account} password=${password}`)
-      if (peoples.get(account)) {
-        ctx.response.body = `
-        <h1>Sign up failed</h1>
-        <h2>Account has been used</h2>`
-      } else {
-        peoples.set(account, {account, password})
-        ctx.response.type = 'text/html'
-        ctx.response.body = `
-        <h1>Sign up success</h1>
-        <h2><a href="/public/login.html">Logging to the system</a></h2>`
-      }
-    }
-  });
-  
 
+    .get('/people',(ctx) => {
+        ctx.response.body = Array.from(peoples.values());
+    })
+
+    .post('/people/signup',async(ctx) => {
+        const body = ctx.request.body()
+        if (body.type === "form") {
+          const pairs = await body.value
+          console.log('pairs=', pairs)
+          const params = {}
+          for (const [key, value] of pairs) {
+            params[key] = value
+          }
+          console.log('params=', params)
+          let name = params['name']
+          let account = params['account']
+          let password = params['password']
+          console.log(`account=${account} password=${password}`)
+          if (peoples.get(account)) {
+            ctx.response.type = 'text/html'
+            ctx.response.body = `<p>error: ${account} Already Exist！</p><br/><a href="http://127.0.0.1:8000/public/signup.html">SignUp</a>`
+          } 
+          else {
+            peoples.set(name, {name, account, password})
+            ctx.response.type = 'text/html'
+            ctx.response.body = `<p>${name}SignUp Success</p><a href="http://127.0.0.1:8000/public/login.html">Login</a>`
+          }
+      
+        }
+      })
+
+    .post('/people/login', async(ctx) =>{
+        const body = ctx.request.body()
+        if (body.type === "form") {
+          const pairs = await body.value
+          console.log('pairs=', pairs)
+          const params = {}
+          for (const [key, value] of pairs) {
+            params[key] = value
+          }
+          console.log('params=', params)
+          let name = params['name']
+          let password = params['password']
+          console.log(`name=${name} password=${password}`)
+          if (peoples.get(name) && password == peoples.get(name).password) {
+            ctx.response.type = 'text/html'
+            ctx.response.body = `<p>Login Success</p>`
+          } 
+          else {
+            ctx.response.type = 'text/html'
+            ctx.response.body = `<p>Login Failed</p><a href="http://127.0.0.1:8000/public/login.html">Login Again</a>`
+          }
+          console.log("key:",peoples.get(name).password)
+        }
+      })
+
+    .get("/public/(.*)", async (ctx) => {
+        let wpath = ctx.params[0]
+        console.log('wpath=', wpath)
+        await send(ctx, wpath, {
+          root: Deno.cwd()+"/public/",
+          index: "index.html",
+        })
+      })
+      
 const app = new Application();
-
 app.use(router.routes());
 app.use(router.allowedMethods());
 
 console.log('start at : http://127.0.0.1:8000')
-
 await app.listen({ port: 8000 });
